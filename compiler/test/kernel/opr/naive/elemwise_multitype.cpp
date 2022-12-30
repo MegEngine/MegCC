fd1d6474594e86442674f115e485b2c9366de677
Megvii Engine Team <megengine@megvii.com>
2022-12-30 17:58:52 +0800

    feat(compiler): add qrelu mode of elemwisemultitype
    GitOrigin-RevId: 8f90921090d2fae73adee2938afe8bf9a94af19e

--- compiler/test/kernel/opr/naive/elemwise_multitype.cpp
+++ compiler/test/kernel/opr/naive/elemwise_multitype.cpp
@@ -12,6 +12,22 @@ using namespace megdnn;
 using namespace megcc::test;
 using MODE = ElemwiseMultiType::Param::Mode;
 
+TEST(NAIVE, ElementwiseMultitypeUnary) {
+    Checker<ElemwiseMultiType> checker;
+    checker.set_kernel_symbol("kernel_.*");
+    checker.set_epsilon(1e-4);
+    checker.set_dtype(0, dtype::QuantizedS8(1.f));
+    checker.set_dtype(1, dtype::QuantizedS8(2.f));
+    ElemwiseMultiType::Param param;
+    for (auto mode : {MODE::QRELU}) {
+        param.mode = mode;
+        checker.set_param(param);
+        checker.execs({{1}, {}});
+        checker.execs({{1, 10}, {}});
+        checker.execs({{1, 10, 12, 13}, {}});
+    }
+}
+
 TEST(NAIVE, ElementwiseMultitypeBinary) {
     Checker<ElemwiseMultiType> checker;
     checker.set_kernel_symbol("kernel_.*");
