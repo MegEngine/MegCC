564438ca4ac56180f3a7559ccfeb7c51dfa69f6b
yuxiongxiong <yuxiongxiong@megvii.com>
2023-01-13 12:02:05 +0800

    feat(ci):recovery copybara

---
+++ ci/copybara.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -bex
+
+PROJECT_DIR=$(readlink -f "$(dirname "$0")/../")
+MEGCC_WORKSPACE_DIR=${PROJECT_DIR}/ci/workdir
+megcc_export_copybara_branch='copybara'
+
+readonly MEGCC_URL="git@git-core.megvii-inc.com:brain-sdk/megcc.git"
+readonly MEGCC_EXPORT_URL="git@github.com:MegEngine/MegCC.git"
+readonly MEGCC_LOCAL_URL="file://${MEGCC_WORKSPACE_DIR}"
+readonly COPYBARA_CONFIG="./migrate/copy.bara.sky"
+
+function pull_megcc_export(){
+    echo "Starting pull megcc_export"
+    if [ -d "${MEGCC_WORKSPACE_DIR}/MegCC" ]; then
+        rm -rf ${MEGCC_WORKSPACE_DIR}/MegCC
+    fi
+    if [ ! -d "${MEGCC_WORKSPACE_DIR}" ]; then
+        mkdir -p ${MEGCC_WORKSPACE_DIR}
+    fi
+    pushd "${MEGCC_WORKSPACE_DIR}" > /dev/null
+        git clone -b ${megcc_export_copybara_branch} "${MEGCC_EXPORT_URL}"
+        git -C "${MEGCC_WORKSPACE_DIR}/MegCC" config receive.denyCurrentBranch ignore
+    popd > /dev/null
+    echo "Finished pulling megcc_export"
+}
+
+
+function do_copybara(){
+    pushd "${PROJECT_DIR}" > /dev/null
+        ./migrate/copybara ${COPYBARA_CONFIG} default \
+            --git-destination-url=${MEGCC_LOCAL_URL}/MegCC \
+            --git-destination-fetch="${megcc_export_copybara_branch}" \
+            --git-destination-push="${megcc_export_copybara_branch}" \
+            --init-history --force --verbose
+
+    popd > /dev/null
+}
+
+pull_megcc_export
+do_copybara
+git -C "${MEGCC_WORKSPACE_DIR}/MegCC" checkout copybara
+git -C "${MEGCC_WORKSPACE_DIR}/MegCC" reset --hard HEAD  
