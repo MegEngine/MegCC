bd60c4f4aea11e8e7a1559ba31fcf287dde3e9dc
Megvii Engine Team <megengine@megvii.com>
2023-04-27 14:59:16 +0800

    fix(test): Merge tests for the arm common kernel from 'arm' and 'armv7' to 'arm_common'
    GitOrigin-RevId: d84d3d83a67c2a014e510cda4dd99b252cdca4e9

--- compiler/test/kernel/opr/arm_common/benchmark_conv.cpp
+++ compiler/test/kernel/opr/arm_common/benchmark_conv.cpp
@@ -55,4 +55,59 @@ TEST(ARMCOMMON, BenchmarkChannelWiseNCHW4Int8) {
     run();
 }
 
+TEST(ARMCOMMON, BenchmarkChannelWiseNCHW4) {
+#ifdef __aarch64__
+    Benchmarker<ConvBiasForward> benchmarker(Arch::ARM64);
+#else
+    Benchmarker<ConvBiasForward> benchmarker(Arch::ARMV7);
+#endif
+    ConvBiasForward::Param param;
+    param.pad_h = 1;
+    param.pad_w = 1;
+    param.stride_h = 1;
+    param.stride_w = 1;
+    param.compute_mode = ConvBiasForward::Param::ComputeMode::DEFAULT;
+    param.format = ConvBiasForward::Param::Format::NCHW44;
+    param.sparse = ConvBiasForward::Param::Sparse::GROUP;
+    benchmarker.set_param(param);
+
+    benchmarker.set_before_exec_callback(
+            megdnn::test::AlgoChecker<ConvBiasForward>("F32_CHANNEL_WISE_NCHW44"));
+    for (size_t k : {3, 5})
+        for (size_t h : {112, 56, 28, 14}) {
+            for (size_t channel : {32, 64}) {
+                auto result = benchmarker.execs(
+                        {{1, channel, h, h, 4},
+                         {channel, 1, 1, k, k, 4},
+                         {1, channel, 1, 1, 4},
+                         {},
+                         {}});
+                printf("Bench kernel %zu channel=%zu, hxw=%zux%zu\n", k, channel, h, h);
+                result.print();
+            }
+        }
+}
+
+TEST(ARMCOMMON, BenchmarkConvNCHWNCHW44) {
+#ifdef __aarch64__
+    Benchmarker<ConvBiasForward> benchmarker(Arch::ARM64);
+#else
+    Benchmarker<ConvBiasForward> benchmarker(Arch::ARMV7);
+#endif
+    benchmarker.set_kernel_symbol("ArmCommon_.+");
+    ConvBiasForward::Param param;
+    param.pad_h = 1;
+    param.pad_w = 1;
+    param.stride_h = 2;
+    param.stride_w = 2;
+    param.compute_mode = ConvBiasForward::Param::ComputeMode::DEFAULT;
+    param.format = ConvBiasForward::Param::Format::NCHW44;
+    benchmarker.set_param(param);
+    benchmarker.execs({{1, 3, 224, 224}, {8, 3, 3, 3, 4}, {1, 8, 1, 1, 4}, {}, {}})
+            .print();
+    benchmarker.execs({{2, 3, 256, 160}, {6, 3, 3, 3, 4}, {1, 6, 1, 1, 4}, {}, {}})
+            .print();
+    ;
+}
+
 #endif
