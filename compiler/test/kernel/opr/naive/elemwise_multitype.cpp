eb91db8c0c3067c3eeedb21128203185bf3e3708
Megvii Engine Team <megengine@megvii.com>
2024-03-15 15:40:45 +0800

    Revert "feat(kernel): add naive QMUL/qsi32xqsi32=qsi8 elemwise multi type kernel"
    This reverts commit 622b670c16e07781741c07c114f8d0bd6a70837b.
    
    GitOrigin-RevId: c96331c82924167f68a9a7a4aad6ad5ffc382c8c

--- compiler/test/kernel/opr/naive/elemwise_multitype.cpp
+++ compiler/test/kernel/opr/naive/elemwise_multitype.cpp
@@ -28,26 +28,11 @@ TEST(NAIVE, ElementwiseMultitypeBinary) {
     checker.set_dtype(2, dtype::QuantizedS8(3.f));
     ElemwiseMultiType::Param param;
 
-    for (auto mode : {MODE::QADD, MODE::QFUSE_ADD_RELU, MODE::QMUL}) {
+    for (auto mode : {MODE::QADD, MODE::QFUSE_ADD_RELU}) {
         param.mode = mode;
         checker.set_param(param);
         checker.execs({{1}, {1}, {}});
         checker.execs({{1, 10}, {1, 10}, {}});
         checker.execs({{2, 3, 4, 5}, {2, 3, 4, 5}, {}});
-        checker.execs({{2, 3, 4, 5}, {1, 3, 1, 1}, {}});
-        checker.execs({{2, 3, 4, 5}, {1}, {}});
-    }
-
-    checker.set_dtype(0, dtype::QuantizedS32(1.f));
-    checker.set_dtype(1, dtype::QuantizedS32(2.f));
-
-    for (auto mode : {MODE::QADD, MODE::QFUSE_ADD_RELU, MODE::QMUL}) {
-        param.mode = mode;
-        checker.set_param(param);
-        checker.execs({{1}, {1}, {}});
-        checker.execs({{1, 10}, {1, 10}, {}});
-        checker.execs({{2, 3, 4, 5}, {2, 3, 4, 5}, {}});
-        checker.execs({{2, 3, 4, 5}, {1, 3, 1, 1}, {}});
-        checker.execs({{2, 3, 4, 5}, {1}, {}});
     }
 }
